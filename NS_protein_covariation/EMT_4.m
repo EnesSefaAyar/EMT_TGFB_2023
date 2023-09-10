5e66b334c6d89635bd1b8444859315a4a6554b1e
Nikolai Slavov <nslavov@alum.mit.edu>
2023-09-10 07:31:54 -0400

    Update EMT_4.m

--- NS_protein_covariation/EMT_4.m
+++ NS_protein_covariation/EMT_4.m
@@ -64,15 +64,20 @@ Rd = rd( in_NAN, in_NAN );
 % Select correlations without missing data and large difference 
 SumR = sum( Rd.^2 );
 in = SumR > prctile( SumR, 50 ); 
-fprintf( 'Number of selected proteins: %d, \n\n', sum(in) );
+fprintf( 'Number of selected proteins: %d \n\n', sum(in) );
 ProtIDs2 = ProtIDs( in_NAN(in) );
+In = in_NAN(in);
 
 % Cluster the matrix of correlation differences 
 Clu = clu( Rd(in,in), 3); rb(1);
 %%
+Kmeans = 1;
 
-In = in_NAN(in);
-In = In(Idx); %(Clu);
+if Kmeans == 1
+    In = In(Idx); 
+else
+    In = In(Clu);
+end
 
 
 Lim = 1;
@@ -138,21 +143,91 @@ fprf( ProtIDs2(flip(Clu,2)), 3, [path 'NS/txt/Prot_CorrClusters_flip.txt']  );
 %% Cluster the matrix of correlation differences using K-means
 
 Rd2 = Rd(in,in);
-[idx,C] = kmeans( Rd2, 3); 
+[idx,C] = kmeans( [Rd2 r1(In,In) r3(In,In)], 3); %
 
 Idx = [find(idx==1); find(idx==2);  find(idx==3) ];
 
 imagesc( Rd2(Idx,Idx) );  rb(1);
 
 %%
-fprf( ProtIDs2, 3, [path 'NS/txt/Prot_CorrClusters_Background.txt']  );
-fprf( ProtIDs2(idx==1), 3, [path 'NS/txt/Prot_CorrCluster-1.txt']  );
-fprf( ProtIDs2(idx==2), 3, [path 'NS/txt/Prot_CorrCluster-2.txt']  );
-fprf( ProtIDs2(idx==3), 3, [path 'NS/txt/Prot_CorrCluster-3.txt']  );
+fprf( ProtIDs2, 3, [path 'NS/txt/b/Prot_CorrClusters_Background.txt']  );
+fprf( ProtIDs2(idx==1), 3, [path 'NS/txt/b/Prot_CorrCluster-1.txt']  );
+fprf( ProtIDs2(idx==2), 3, [path 'NS/txt/b//Prot_CorrCluster-2.txt']  );
+fprf( ProtIDs2(idx==3), 3, [path 'NS/txt/b/Prot_CorrCluster-3.txt']  );
+
+
+%% Display mean correlations for each cluster across time 
+clear h, close all
+cMean =  zeros( 3 ); 
+for i = 1:3
+    ci = In(idx==i);
+    
+    cMean(1,i) = mean( squareform( r1(ci,ci)-diag(diag(r1(ci,ci))), 'tovector') ); 
+    cMean(2,i) = mean( squareform( r2(ci,ci)-diag(diag(r2(ci,ci))), 'tovector') ); 
+    cMean(3,i) = mean( squareform( r3(ci,ci)-diag(diag(r3(ci,ci))), 'tovector') ); 
+end
+
+plot( 1:3, cMean, 'o-', 'MarkerSize', 16, 'Linewidth', 5 );
+xlim( [0.5 3.9]);
+ylim( [0.05 0.42]);
+grid minor
+set(gca, 'Xtick', 1:3, 'XtickLabel', {'Day 0', 'Day 3', 'Day 9'}, 'FontSize', 32);
+set(gca, 'Ytick', 0.1: 0.1 :0.4);
+h(1) = ylabel( 'Mean correlation');
+h(2) = legend( 'c2  ', 'c1   ', 'c3');
+%set(h(2), 'Orientation', 'horizontal' );
+set(h, 'FontSize', 40 );
+%%
+pdf( [path 'NS/PDFs/Mean-cluster-correlation'], [9 6], 1 );
 
 
 
+%% Display mean protein abundance for each cluster across time 
+clear h, close all
+aMean =  zeros( 3 ); 
+for i = 1:3
+    ci = In(idx==i);
+    
+    aMean(1,i) = mean(nanmean( dat_Lin(ci, ins == 1)  )); 
+    aMean(2,i) = mean(nanmean( dat_Lin(ci, ins == 2)  )); 
+    aMean(3,i) = mean(nanmean( dat_Lin(ci, ins == 3)  )); 
+end
 
+plot( 1:3, aMean, 'o-', 'MarkerSize', 16, 'Linewidth', 5 );
+xlim( [0.5 3.9]);
+ylim( [-0.2 0.2]);
+grid minor
+set(gca, 'Xtick', 1:3, 'XtickLabel', {'Day 0', 'Day 3', 'Day 9'}, 'FontSize', 32);
+set(gca, 'Ytick', -0.2: 0.1 :0.2 );
+h(1) = ylabel( 'Mean abundance');
+h(2) = legend( 'c2  ', 'c1   ', 'c3');
+%set(h(2), 'Orientation', 'horizontal' );
+set(h, 'FontSize', 40 );
+%%
+pdf( [path 'NS/PDFs/Mean-cluster-abundance'], [9 6], 1 );
+
+
+%% Display correlation vs mean protein abundance 
+clear h, close all
+r_mean_corr_1 = pear2i( aMean', cMean' )
+
+[r_mean_corr_2, p_val] = corr( aMean(:), cMean(:) )
+
+plot( cMean, aMean, 'o', 'MarkerSize', 16, 'Linewidth', 5 );
+
+xlim( [0.05 0.42]);
+ylim( [-0.2 0.2]);
+grid minor
+set(gca, 'FontSize', 32);
+set(gca, 'Ytick', -0.2: 0.1 :0.2 );
+h(1) = ylabel( 'Mean abundance');
+h(2) = xlabel( 'Mean correlation');
+h(3) = legend( 'c2  ', 'c1   ', 'c3');
+h(4) = text( 0.1, 0.15, sprintf( '$r = %1.2f$', r_mean_corr_2 ) );
+set(h(4), 'interpreter', 'latex' );
+set(h, 'FontSize', 40 );
+%%
+pdf( [path 'NS/PDFs/Mean-clustercorr-vs--abundance'], [9 6], 1 );
 
 
 
